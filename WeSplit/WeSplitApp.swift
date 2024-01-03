267993286e43065449358795546cbf7724a7dc01
Alvin Reyvaldo <alvinreyvaldo1@gmail.com>
2024-01-03 21:12:49 +0700

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by temp on 03/01/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
